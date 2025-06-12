01d13eb1890c9940b61d05e48dff640d2304b208
apoloxz-blip <irvinggaleno@gmail.com>
2025-06-12 02:28:01 -0300

    Update MusicPlayer.js

--- componente/MusicPlayer.js
+++ componente/MusicPlayer.js
@@ -1,20 +1,24 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function MusicPlayer() {
   const audioRef = useRef(null);
+  const [autorizado, setAutorizado] = useState(false);
+
+  useEffect(() => {
+    const permitido = localStorage.getItem('musicaPermitida');
+    if (permitido === 'true') {
+      setAutorizado(true);
+    }
+  }, []);
 
   useEffect(() => {
     const audio = audioRef.current;
-    if (audio) {
-      const play = () => {
-        audio.play().catch(() => {});
-      };
-      document.addEventListener('click', play, { once: true });
-      return () => document.removeEventListener('click', play);
+    if (autorizado && audio) {
+      audio.play().catch(() => {});
     }
-  }, []);
+  }, [autorizado]);
 
   return (
     <audio ref={audioRef} loop>
